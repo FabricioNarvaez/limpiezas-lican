e42495d8c120cadc62cc57df8c3cb9c7a5e8430d
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-09-29 19:40:43 +0200

    Setup tailwind custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    
+    theme: {
+        fontFamily: {
+        sans: ['Inter', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+        },
+        extend: {
+        colors: {
+            'fondo': '#FAFAFA',
+            'texto-principal': '#1E1E1E',
+            'texto-secundario': '#4B5563',
+            
+            'acento': {
+            DEFAULT: '#047857',
+            'claro': '#34D399',
+            'oscuro': '#064E3B',
+            },
+            
+            'contraste': '#0070F3',
+        },
+        spacing: {
+            '128': '32rem',
+            '144': '36rem',
+        },
+        boxShadow: {
+            'sutil': '0 4px 6px rgba(0, 0, 0, 0.05), 0 1px 3px rgba(0, 0, 0, 0.03)',
+            'elevado': '0 10px 15px -3px rgba(0, 0, 0, 0.08), 0 4px 6px -2px rgba(0, 0, 0, 0.04)',
+        }
+        },
+    },
+    
+    plugins: [],
 }
-
