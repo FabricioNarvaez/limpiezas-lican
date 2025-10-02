8e3ec4a8200519451c4234d181ab40d4ff2c8dbc
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-10-02 18:47:32 +0200

    Formkit installed

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { Icon } from '@iconify/vue'
+import { plugin, defaultConfig } from '@formkit/vue'
 import '@assets/main.css'
 import App from './App.vue'
 import router from '@router/index.js'
-import { createPinia } from 'pinia'
-import { Icon } from '@iconify/vue';
+
 
 createApp(App)
 .use(router)
+.use(plugin, defaultConfig({
+    theme: 'genesis'
+}))
 .component('Icon', Icon)
 .use(createPinia())
 .mount('#app')
