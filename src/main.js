eff836824e7f10c4339e390ec93c83e88638a1f5
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-10-07 12:38:24 +0200

    Import formkit styles

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { Icon } from '@iconify/vue'
 import { plugin, defaultConfig } from '@formkit/vue'
-import '@assets/main.css'
 import App from './App.vue'
 import router from '@router/index.js'
+import '@assets/main.css'
+import '@formkit/themes/genesis' 
 
 
 createApp(App)
