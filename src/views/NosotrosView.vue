2eb7cc6003323daeb9f20e14b47d98c49926b24e
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-10-08 11:10:40 +0200

    Create NosotrosView with header

---
+++ src/views/NosotrosView.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+        <ViewHeader :headerProps />
+
+        <!-- <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-16 md:py-24">
+            <ValueProposition />
+        </div>
+
+        <div class="bg-acento-claro py-16 md:py-24">
+            <div class="container mx-auto px-4 sm:px-6 lg:px-8">
+                <EcologicalCommitment />
+            </div>
+        </div>
+
+        <div class="container mx-auto px-4 sm:px-6 lg:px-8 pt-16 pb-24">
+            <TrustFigures />
+        </div> -->
+    </main>
+</template>
+
+<script setup>
+    import ViewHeader from '@components/viewHeader.vue';
+// import ValueProposition from '@/components/ValueProposition.vue';
+// import EcologicalCommitment from '@/components/EcologicalCommitment.vue';
+// import TrustFigures from '@/components/TrustFigures.vue';
+
+    const headerProps = {
+        title: 'Sobre Nosotros',
+        subtitle: 'Lican: Limpieza con',
+        subtitleSpan: 'Conciencia y Calidad',
+        description: 'Aunque Lican acaba de nacer, lo hace con la experiencia y el compromiso personal de nuestro equipo, el cual traslada más de 10 años en el sector. Clientes de confianza nos han seguido, buscando un servicio más directo y responsable.'
+    }
+</script>
