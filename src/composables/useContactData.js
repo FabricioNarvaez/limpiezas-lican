1fa5d2b2fbb760c5893b74a608e2f0536e7f333e
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-10-11 10:23:57 +0200

    Move contact data to a composable and use on privacyView

---
+++ src/composables/useContactData.js
@@ -0,0 +1,8 @@
+export const contactInfo = {
+    owner: 'Ramón Ramiro Chávez Orozco',
+    cif: '73617556F',
+    phone: '+34 675 47 60 14',
+    email: 'contacto@limpiezaslican.es',
+    address: 'Paseo Anelier 6, 31014 Pamplona, España',
+    website: 'www.limpiezaslican.es',
+}
