7fc31fbe172699cc82d2f84c4900c97978b427ef
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-10-20 11:55:58 +0200

    Add file to generate sitemap on build

---
+++ generate-sitemap.js
@@ -0,0 +1,42 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { writeFileSync } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const hostname = 'https://limpiezaslican.es/'; 
+const outputDir = 'dist'; 
+const links = [
+    { url: '/', changefreq: 'weekly', priority: 1.0 },
+    { url: '/servicios', changefreq: 'monthly', priority: 0.9 },
+    { url: '/contacto', changefreq: 'monthly', priority: 0.8 },
+    { url: '/nosotros', changefreq: 'monthly', priority: 0.7 },
+    { url: '/legal/politica-privacidad', changefreq: 'yearly', priority: 0.5 },
+    { url: '/legal/aviso-legal', changefreq: 'yearly', priority: 0.5 },
+    { url: '/legal/politica-cookies', changefreq: 'yearly', priority: 0.5 },
+];
+
+const generateSitemap = async () => {
+    try {
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        const sitemapPath = join(__dirname, outputDir, 'sitemap.xml');
+        
+        const sitemapStream = new SitemapStream({ hostname });
+        
+        links.forEach(link => {
+            sitemapStream.write(link);
+        });
+        sitemapStream.end();
+
+        const sitemapContent = await streamToPromise(sitemapStream);
+        
+        writeFileSync(sitemapPath, sitemapContent);
+
+        console.log(`✅ Sitemap.xml generado exitosamente en: ${sitemapPath}`);
+        
+    } catch (error) {
+        console.error('❌ Error fatal al generar el sitemap:', error);
+        process.exit(1); 
+    }
+};
+
+generateSitemap();
